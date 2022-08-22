723ef59db1a4ce94c95aea26621ac094b124c32d
Rachel Rybarczyk <rj@rybar.tech>
2022-08-22 16:46:22 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn ret_two() -> u8 {
+    2u8
+}
+fn ret_one() -> u8 {
+    1u8
+}
+
+fn main() {
+    println!("One: {}", ret_one());
+    println!("Two: {}", ret_two());
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ret_one() {
+        assert_eq!(1, ret_one());
+    }
+}
